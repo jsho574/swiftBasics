9471ec49b6f03962f71393f722d745727880e4e8
Joshua McEwen <jamyt7@mail.missouri.edu>
2019-02-08 17:57:31 -0600

    First commet

---
+++ swiftBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftBasics
+//
+//  Created by McEwen, Joshua A. (MU-Student) on 2/8/19.
+//  Copyright © 2019 McEwen, Joshua A. (MU-Student). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
