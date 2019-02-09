bd9887f729d0ad1ee17a4c5357b5ae5c6f54a86f
Joshua McEwen <jamyt7@mail.missouri.edu>
2019-02-08 19:07:25 -0600

    finished up to the directions

--- swiftBasics/main.swift
+++ swiftBasics/main.swift
@@ -34,40 +34,54 @@ var name: String
 name = "Karen"
 
 if sample1 == sample2 {
-    print("the samples are equal")
+    print("the samples are equal\n")
 } else {
-    print("the samples are not equal")
+    print("the samples are not equal\n")
     }
 
 if heartRate >= 40 && heartRate <= 80 {
-    print("Heart rate is normal")
+    print("Heart rate is normal\n")
 }
 
 if deposits >= 100000000 {
-    print("You are exceedinly wealthy")
+    print("You are exceedinly wealthy\n")
+} else {
+    print("Sorry you are so poor\n")
 }
 
 var force: Float
 force = mass * acceleration
-print("Force = \(force)")
-print("\(distance) is the distance")
+print("Force = \(force)\n")
+print("\(distance) is the distance\n")
 
 if lost == true && expensive == true {
-    print("I am really sorry! I will get the manager")
+    print("I am really sorry! I will get the manager\n")
 }
 if lost == true && expensive == false {
-    print("Here is a coupon for 10% off")
+    print("Here is a coupon for 10% off\n")
 }
 
 switch choice{
 case 1:
-    print("You chose 1")
+    print("You chose 1\n")
 case 2:
-    print("You chose 2")
+    print("You chose 2\n")
 case 3:
-    print("You chose 3")
+    print("You chose 3\n")
 default:
-    print("You made an unknown choice")
+    print("You made an unknown choice\n")
 }
 
+print("\(integral) is an integral\n")
 
+for i in 5...10{
+    print("i = \(i)\n")
+}
+
+var age: Int
+age = 0
+while age < 6{
+    print("age = \(age)\n")
+    age += 1
+}
+print("\(greeting)\n\(name)")
