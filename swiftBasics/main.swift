98496e5da7a8fb6783bbd535183c8d9436409083
Joshua McEwen <jamyt7@mail.missouri.edu>
2019-02-08 18:26:10 -0600

    added the varibles and contants

--- swiftBasics/main.swift
+++ swiftBasics/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
+let smaple1 = 0x3A
+var sample2 = 58
+var heartRate: UInt8
+heartRate = 85
+var deposits: Double
+deposits = 135002796
+let acceleration: Float
+acceleration = 9.800
+var mass: Float
+mass = 14.6
+var distance: Double
+distance = 129.763001
+var lost: Bool
+lost = true
+var expensive: Bool
+expensive = true
+var choice: Int
+choice = 2
+let integral: Character
+integral = "\u{222B}"
+let greeting: String
+greeting = "Hello"
+var name: String
+name = "Karen"
+
+
 
 
